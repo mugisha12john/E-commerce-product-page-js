91b02f8f9d6f00fa79ba5963a9a23541bd559070
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-07 15:25:01 +0200

    feat: toggle menu bar using js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-document.body.style.backgroundColor = "red";
+const menuBtn = document.getElementById("menu-btn");
+const menuList = document.getElementById("toggle-menu-bar");
+const closeBtn = document.getElementById("close-btn");
+menuBtn.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+  document.body.style.overflow = "hidden";
+});
+closeBtn.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+  document.body.style.overflow = "visible";
+});
