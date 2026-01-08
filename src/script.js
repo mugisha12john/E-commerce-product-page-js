9e9062a049e608c1234f0dae1237d658a122baf5
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-08 15:56:00 +0200

    feat: toggle cart using js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById("menu-btn");
 const menuList = document.getElementById("toggle-menu-bar");
 const closeBtn = document.getElementById("close-btn");
+const cartBtn = document.getElementById("cart-btn");
+const cartItems = document.getElementById("cart-items");
 menuBtn.addEventListener("click", () => {
   menuList.classList.toggle("hidden");
   document.body.style.overflow = "hidden";
@@ -8,4 +10,11 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   menuList.classList.toggle("hidden");
   document.body.style.overflow = "visible";
+});
+cartBtn.addEventListener("click", () => {
+  if (cartItems.classList.toggle("hidden")) {
+    document.body.style.overflow = "visible";
+  } else {
+    document.body.style.overflow = "hidden";
+  }
 });
