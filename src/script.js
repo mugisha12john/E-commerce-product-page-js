7870ae8d23d355830db72de0033b78853ecbc4cb
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-18 09:49:56 +0200

    feat: change image background in hero section using js

--- src/script.js
+++ src/script.js
@@ -24,3 +24,54 @@ bgcolor.addEventListener("click", () => {
 closeLightbox.addEventListener("click", () => {
   lightbox.classList.toggle("hidden");
 });
+
+// images
+const image = [
+  {
+    id: 0,
+    desktop: "images/image-product-1.jpg",
+    mobile: "images/image-product-1-thumbnail.jpg",
+  },
+  {
+    id: 1,
+    desktop: "images/image-product-2.jpg",
+    mobile: "images/image-product-2-thumbnail.jpg",
+  },
+  {
+    id: 2,
+    desktop: "images/image-product-3.jpg",
+    mobile: "images/image-product-3-thumbnail.jpg",
+  },
+  {
+    id: 3,
+    desktop: "images/image-product-4.jpg",
+    mobile: "images/image-product-4-thumbnail.jpg",
+  },
+];
+
+let currentIndex = 0;
+const mobileNextBtn = document.getElementById("mobile-next-btn");
+const mobilePrevBtn = document.getElementById("mobile-prev-btn");
+const heroSection = document.getElementById("hero-section");
+function mobileNext() {
+  currentIndex = (currentIndex + 1) % image.length;
+}
+function mobilePrev() {
+  currentIndex = (currentIndex - 1) % image.length;
+  if (currentIndex < 0) {
+    return 0;
+  } else {
+    currentIndex;
+  }
+}
+function showImage(index, screen) {
+  heroSection.setAttribute("src", image[index][screen]);
+}
+mobileNextBtn.addEventListener("click", () => {
+  mobileNext();
+  showImage(currentIndex, "desktop");
+});
+mobilePrevBtn.addEventListener("click", () => {
+  mobilePrev();
+  showImage(currentIndex, "desktop");
+});
